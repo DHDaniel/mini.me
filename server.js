2f9ba98d26b70bd764b08dbaa95e6edb532537e3
Daniel Hernandez <dh.danny@hotmail.com>
2016-10-16 22:09:37 -0400

    Assets created. Begin working on homepage

--- server.js
+++ server.js
@@ -8,14 +8,41 @@ const shortid = require("shortid");
 
 var app = express();
 
+
+/*
+* CONFIGURATION
+*/
+
 // assigning handlebars engine to app
-app.engine("html", cons.handlebars);
+app.engine("html", consolidate.handlebars);
 
 // telling it templates are identified by .html
 app.set("view engine", "html");
 app.set("views", __dirname + "/views");
 
+// where to find static files
+app.use(express.static(__dirname + "/static"));
+
+// allowing Heroku to set up its own port
+app.set("port", (process.env.PORT || 8080));
+
+
+/*
+* ROUTING
+*/
+
 app.get("/", function (req, res) {
 
-  res.end("Welcome");
+  res.render("index", {
+
+  });
+});
+
+
+/*
+* INITIALISING
+*/
+
+app.listen(app.get("port"), function () {
+  console.log("Listening on port", app.get("port"));
 });
